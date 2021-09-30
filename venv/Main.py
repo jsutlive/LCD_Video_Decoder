184a3a51f6f2bbd673d0a3d3b91c2cc5dd5c4935
jsutlive <jsutlive@gmail.com>
2021-09-30 14:57:24 -0400

    Fixing code

--- venv/Main.py
+++ venv/Main.py
@@ -2,7 +2,7 @@ from imutils.perspective import four_point_transform
 from imutils import contours
 import imutils
 import cv2
-
+import os
 #describe each number on a seven-segment display as a series of on/off switches
 DIGITS_LOOKUP = {
     (1,1,1,0,1,1,1) : 0,
@@ -16,6 +16,10 @@ DIGITS_LOOKUP = {
     (1,1,1,1,1,1,1) : 8,
     (1,1,1,1,0,1,1) : 9
 }
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+vidcap = cv2.VideoCapture('testVideo.mp4')
+success, image = vidcap.read()
 
 def main():
     print('Hello World');
