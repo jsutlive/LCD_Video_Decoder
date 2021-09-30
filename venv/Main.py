73cc69a8ac16841fdda042c685829b98aba5ddf0
jsutlive <jsutlive@gmail.com>
2021-09-30 16:17:08 -0400

    Selecting image

--- venv/Main.py
+++ venv/Main.py
@@ -1,4 +1,5 @@
 from imutils.perspective import four_point_transform
+import matplotlib.pyplot as plt
 from imutils import contours
 import imutils
 import cv2
@@ -18,11 +19,36 @@ DIGITS_LOOKUP = {
 }
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-vidcap = cv2.VideoCapture('testVideo.mp4')
+vidcap = cv2.VideoCapture(ROOT_DIR + '\\testVideo.mp4')
 success, image = vidcap.read()
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray,(5,5),0)
+edged = cv2.Canny(blurred, 50,200,255)
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts,key=cv2.contourArea, reverse=True)
+displayCnt = None
+
+for c in cnts:
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+    if len(approx) == 4:
+        displayCnt = approx
+        break
+
+warped = four_point_transform(gray, displayCnt.reshape(4, 2))
+output = four_point_transform(image, displayCnt.reshape(4, 2))
+
+def showImage(image):
+    plt.imshow(image)
+    plt.show()
 
 def main():
-    print('Hello World');
+    showImage(image)
+    showImage(edged)
+    showImage(warped)
+    showImage(output)
 
 if __name__ == "__main__":
     main();
